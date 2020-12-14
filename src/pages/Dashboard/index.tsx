d850fb42b5155e01c1edf9edd659db66e7c747fc
danieljuniorce <danieljuniorce@hotmail.com>
2020-12-14 10:43:13 -0300

    modified link connect with api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -12,7 +12,8 @@ function Dashboard() {
       <Container>
         <Painel>
           <Title>
-            Sua conta foi criada com Sucesso, entre no nosso servidor.
+            Servidor está em desenvolvimento, então aguarde e acompanhe no
+            discord!.
           </Title>
           <FaCheckCircle size={60} color="#2b961f" />
           <Ip>mtasa://192.168.1.1:27777</Ip>
