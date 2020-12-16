4b2c6fe1c9bc4ea69940c74b17ca0521bac957a2
danieljuniorce <danieljuniorce@hotmail.com>
2020-12-16 18:33:56 -0300

    colorsheme repair

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-import { mode } from '@chakra-ui/theme-tools';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import Routes from './routes';
-import Colors from './components/colors';
-
-const styles = {
-  global: (props: any) => ({
-    body: {
-      color: mode('#000', 'whiteAlpha.900')(props),
-      bg: mode('#D9ADAA', Colors.second)(props),
-      footer: mode('#fff', Colors.third)(props),
-    },
-  }),
-};
-
-const theme = extendTheme({ styles });
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <Routes />
     </ChakraProvider>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { ColorModeScript } from '@chakra-ui/react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ColorModeScript initialColorMode="dark" />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
